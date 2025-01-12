2b81c96d7cc07db6066791ccf65dc5327f4dbcf3
Bagus Pahlefi <baguspahlefi2000@gmail.com>
2025-01-12 11:48:52 +0700

    setup deploy vercel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ImageUploadController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/get', [ImageUploadController::class, 'index']);
+
+Route::post('/customers', [PostController::class, 'store']);
+
+Route::post('/upload', [ImageUploadController::class, 'store']);
